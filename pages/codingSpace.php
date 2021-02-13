21a5cde0f989c4f9b11dd9c9c6aa7c94d705ed54
jearbcpe <teerapon.jearb@gmail.com>
2021-02-13 12:43:28 +0700

    xxx

---
+++ pages/codingSpace.php
@@ -0,0 +1,53 @@
+<div class="card card-custom card-shadowless gutter-b">
+    <div class="card-header border-0 py-5">
+        <h3 class="card-title align-items-start flex-column">
+            <div class="col-12">
+                <span class="card-label font-weight-bolder text-dark">Coding space</span>
+            </div>
+            <!--<span class="text-muted mt-3 font-weight-bold font-size-sm">More than 400+ new members</span>-->
+        </h3>
+
+    </div>
+    <div class="card-body">
+        <div class="card card-custom card-shadowless gutter-b">
+            <div class="col-12">
+                <div class="col-12">
+                    <div class="form-group row">
+                        <textarea class="form-control" rows="5" id="txtCode"></textarea>
+                    </div>
+                    <div class="form-group row">
+                        <button type="button" onclick="run();" class="btn btn-lg btn-outline-success mr-2">Run</button>
+                        <button type="button" onclick="clearCode();" class="btn btn-lg btn-outline-danger mr-2">Clear</button>
+
+                    </div>
+                    <div class="form-group row">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    function run() {
+        var el = document.getElementById('txtCode');
+        var scriptText = el.value;
+        var oldScript = document.getElementById('scriptContainer');
+        var newScript;
+
+        if (oldScript) {
+            oldScript.parentNode.removeChild(oldScript);
+        }
+
+        newScript = document.createElement('script');
+        newScript.id = 'scriptContainer';
+        newScript.text = el.value;
+        document.body.appendChild(newScript);
+        document.getElementById('txtCode').focus();
+    }
+
+    function clearCode() {
+        document.getElementById('txtCode').value = "";
+        document.getElementById('txtCode').focus();
+    }
+</script>
---
+++ pages/javascript/helloJS.php
@@ -0,0 +1,42 @@
+<div class="d-flex flex-column-fluid">
+    <!--begin::Container-->
+    <div class="container">
+        <div class="card card-custom card-shadowless gutter-b">
+            <!--begin::Header-->
+            <!--
+            <div class="card-header border-0 py-5">
+                <h3 class="card-title align-items-start flex-column">
+                    <span class="card-label font-weight-bolder text-dark">JavaScript</span>
+                   <span class="text-muted mt-3 font-weight-bold font-size-sm">More than 400+ new members</span>
+                </h3>
+            </div>
+-->
+            <!--end::Header-->
+
+            <!--begin::Body-->
+            <div class="card-body">
+            <div class="col-12">
+                <code><h2 class="h2 ">เขียนคำสั่งแสดงข้อความ Hello JavaScript</h2></code>
+            </div>
+
+            <div class="col-12 pt-10">
+                <h3 class="display-4" id="headerText">Hello ......</h3>
+            </div>
+              
+            </div>
+            <!--end::Body-->
+        </div>
+        <div class="card card-custom card-shadowless gutter-b">
+            <!--begin::Body-->
+            <div class="card-body">
+            <div class="col-12">
+                <span class="card-label font-weight-bolder text-dark">Coding space</span>
+            </div>
+                <div class="col-6">
+                    <xmp><h3 class="display-4" id="headerText">Hello ......</h3></xmp>
+                </div>
+            </div>
+            <!--end::Body-->
+        </div>
+        <?php include('pages/codingSpace.php'); ?>
+    </div>
